be288940ce07406f382be2b25e152f8f2af2413d
harikrishna2208 <harikrishna2278@gmail.com>
2020-08-22 15:10:15 +0530

    Updates

---
+++ precache-manifest.494e03524f3c66dd408f778252992176.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381cfa576e1c2fdd23932f9c552f89b7",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8686c65b674b4403f6d2",
+    "url": "/weather-app/static/css/main.9d0ea96b.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8686c65b674b4403f6d2",
+    "url": "/weather-app/static/js/main.3ebe1217.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "f4bc2f82dcf539632c123d75713a942a",
+    "url": "/weather-app/static/media/alpsMoun.f4bc2f82.jpg"
+  },
+  {
+    "revision": "560cf610b558dab0d736597d21077fb3",
+    "url": "/weather-app/static/media/houseMoun.560cf610.jpg"
+  }
+]);
